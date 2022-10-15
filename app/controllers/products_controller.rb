aa068934442af1c5445a888a51b8c105cd784f97
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-14 23:55:44 -0500

    New Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,13 @@ class ProductsController < ApplicationController
     def new
         @product = Product.new
     end
+    def create
+        @product = Product.new(product_params)
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:title, :description, :price)
+    end
 end
