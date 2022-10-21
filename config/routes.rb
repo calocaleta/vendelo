8b0e7ccd681cb15094b6dc57e1adeaa9aeb63062
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-20 23:55:43 -0500

    Clean code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-=begin
-  delete '/products/:id', to: 'products#destroy'
-  patch '/products/:id', to: 'products#update'
-  post '/products', to: 'products#create'
-  get '/products/new', to: 'products#new', as: :new_product
-  get '/products', to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
-=end
   resources :products, path: '/'
 end
